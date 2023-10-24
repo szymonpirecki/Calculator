3200ab0c81ce5dcab627b6f9c49b17f07aeb3bb6
szymonpirecki <szymon.pirecki@gmail.com>
2023-10-24 09:11:39 +0200

    [SUP_v6_Java_3.1_Calculator] - correction after code review

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator<Integer> integerCalculator = new Calculator<>();
-        Operation<Integer> minus = (a, b) -> a - b;
-        Operation<Integer> plus = (a, b) -> a + b;
+        Operation<Integer> minus = new Operation<Integer>() {
+            @Override
+            public Integer execute(Integer a, Integer b) {
+                return a - b;
+            }
+        };
+        Operation<Integer> plus = new Operation<Integer>() {
+            @Override
+            public Integer execute(Integer a, Integer b) {
+                return a + b;
+            }
+        };
 
         System.out.println(integerCalculator.performOperation(2, 1, minus));
         System.out.println(integerCalculator.performOperation(2, 1, plus));
